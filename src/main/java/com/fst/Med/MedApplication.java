c3d485178b22593868872e6af401c9bc99008d57
wahidridi <wahidridi@gmail.com>
2025-05-07 00:27:50 +0100

    my first commit

---
+++ src/main/java/com/fst/Med/MedApplication.java
@@ -0,0 +1,13 @@
+package com.fst.Med;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fst/Med/MedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fst.Med;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
